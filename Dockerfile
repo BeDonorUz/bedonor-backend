4604dd34c7ac67d428af57a06cae86d5dc400272
Sardorkhuja <mr.khuzha@gmail.com>
2022-05-28 22:40:31 +0300

    add typeorm

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:18-alpine3.14
 
 WORKDIR /app
+
 RUN npm i -g pnpm
 
 COPY package*.json .
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as config from 'ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config as TypeOrmModuleOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
