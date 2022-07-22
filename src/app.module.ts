d0a2b336a7ee7adcd778b13d5f21e84f301ee879
Sardorkhuja <mr.khuzha@gmail.com>
2022-07-23 01:26:38 +0500

    remove unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import * as config from 'ormconfig';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DonationsModule } from './donations/donations.module';
 import { IdMiddleware } from './utils/middlewares/id.middleware';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config as TypeOrmModuleOptions),
-    UsersModule,
-    PrismaModule,
-    DonationsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PrismaModule, DonationsModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PrismaModule],
+  imports: [PrismaModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
